ca98346e07f7e417592e33647de688545d2fa615
Isaiah (Da-Manager) <damanagerltd@gmail.com>
2019-08-01 02:21:53 +0100

    Location
    Shipping location available UK, EU, & USA

--- DashikiWears/dashikiwears-shipping.php
+++ DashikiWears/dashikiwears-shipping.php
@@ -2,6 +2,8 @@
 /**
  * Plugin Name: DashikiWears
  * Description: DashikiWears is  a woocommerce custom shipping method plugin
+ * Tutorials from https://docs.woocommerce.com/document/shipping-method-api/# 
+ * Skeleton from https://gist.github.com/woogists/c0cb26faa4f329dc0c01d78f53e797e9#file-wc-skeleton-shipping-method-example-php
  */
 if ( ! defined( 'WPINC' ) ){
  die('security by preventing any direct access to your plugin file');
@@ -17,4 +19,14 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
  $this->id = 'dashikiwears';
  $this->method_title = __('dashikiwears Shipping', 'dashikiwears');
  $this->method_description = __('Custom Shipping Method for dashikiwears', 'dashikiwears');
- 
+ // Location availability
+ $this->availability = 'including';
+ $this->countries = array(
+ 'EU',
+ 'GB',
+ 'US',
+ );
+ $this->init();
+ $this->enabled = isset($this->settings['enabled']) ? $this->settings['enabled'] : 'yes';
+ $this->title = isset($this->settings['title']) ? $this->settings['title'] : __('dashikiwears Shipping', 'dashikiwears');
+ }
